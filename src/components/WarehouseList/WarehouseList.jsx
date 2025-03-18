466f36119cdf0439071fe8339377db5a9ad940a0
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-18 17:08:23 +0000

    add warehouse list header styling

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -6,8 +6,15 @@ export default function WarehouseList() {
       <header className="warehouse-list__header">
         <h1 className="warehouse-list__title">Warehouses</h1>
         <div className="warehouse-list__cta-wrapper">
-          <input type="search" name="searchbar" />
-          <button>+ Add New Warehouse</button>
+          <input
+            className="warehouse-list__search-bar"
+            type="search"
+            name="searchbar"
+            placeholder="Search..."
+          />
+          <button className="warehouse-list__button">
+            + Add New Warehouse
+          </button>
         </div>
       </header>
     </section>
