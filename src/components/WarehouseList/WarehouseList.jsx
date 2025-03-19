a2fe279ea2b6a79e39049a92b4c475cc1e6efd94
Isabella Inglis <issy-inglis@hotmail.co.uk>
2025-03-19 11:55:39 +0000

    add layout for table content

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,4 +1,7 @@
 import "./WarehouseList.scss";
+import sortImg from "../../assets/icons/sort-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
 
 export default function WarehouseList() {
   return (
@@ -19,12 +22,61 @@ export default function WarehouseList() {
       </header>
       <div className="warehouse-list__table-headings">
         <ul className="warehouse-list__table-headings-list">
-          <li className="warehouse-list__table-heading">WAREHOUSE</li>
-          <li className="warehouse-list__table-heading">ADDRESS</li>
-          <li className="warehouse-list__table-heading">CONTACT NAME</li>
-          <li className="warehouse-list__table-heading">CONTACT INFORMATION</li>
-          <li className="warehouse-list__table-heading">ACTIONS</li>
+          <li className="warehouse-list__table-heading">
+            WAREHOUSE
+            <img className="warehouse-list__sort-icon" src={sortImg} alt="" />
+          </li>
+          <li className="warehouse-list__table-heading">
+            ADDRESS
+            <img className="warehouse-list__sort-icon" src={sortImg} alt="" />
+          </li>
+          <li className="warehouse-list__table-heading">
+            CONTACT NAME
+            <img className="warehouse-list__sort-icon" src={sortImg} alt="" />
+          </li>
+          <li className="warehouse-list__table-heading">
+            CONTACT INFORMATION{" "}
+            <img className="warehouse-list__sort-icon" src={sortImg} alt="" />
+          </li>
+          <li className="warehouse-list__table-heading">
+            ACTIONS{" "}
+            <img className="warehouse-list__sort-icon" src={sortImg} alt="" />
+          </li>
         </ul>
+      </div>
+      <ul className="warehouse-list__info-container">
+        <div className="warehouse-list__info-col">
+          <li className="warehouse-list__info-item">
+            <p className="warehouse-list__info-subheading">WAREHOUSE</p>
+            <p className="warehouse-list__info">Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li className="warehouse-list__info-item">
+            <p className="warehouse-list__info-subheading">ADDRESS</p>
+            <p className="warehouse-list__info">Lorem ipsum dolor sit amet.</p>
+          </li>
+        </div>
+        <div className="warehouse-list__info-col">
+          <li className="warehouse-list__info-item">
+            <p className="warehouse-list__info-subheading">CONTACT NAME</p>
+            <p className="warehouse-list__info">Lorem ipsum dolor sit amet.</p>
+          </li>
+          <li className="warehouse-list__info-item">
+            <p className="warehouse-list__info-subheading">CONTACT INFO</p>
+            <p className="warehouse-list__info">Lorem ipsum dolor sit amet.</p>
+          </li>
+        </div>
+      </ul>
+      <div className="warehouse-list__icons-wrapper">
+        <img
+          className="warehouse-list__icon"
+          src={deleteIcon}
+          alt="Bin warehouse icon"
+        />
+        <img
+          className="warehouse-list__icon"
+          src={editIcon}
+          alt="Edit warehouse icon"
+        />
       </div>
     </section>
   );
