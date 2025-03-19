47f9111b80fa60975ea733bfc69cda59e696960f
Robell <robellasfaw@gmail.com>
2025-03-19 12:59:49 +0000

    Add styling to Navigation

---
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -0,0 +1,7 @@
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import "./WarehouseDetailsPage.scss";
+
+export default function WarehouseDetailsPage() {
+  return;
+  <WarehouseDetails />;
+}
---
+++ src/pages/WarehousesPage/WarehousesPage.jsx
@@ -0,0 +1,12 @@
+import Navigation from "../../components/Navigation/Navigation";
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import "./WarehousesPage.scss";
+
+export default function WarehousesPage() {
+	return (
+		<>
+			<Navigation />
+			<WarehouseDetails />;
+		</>
+	);
+}
