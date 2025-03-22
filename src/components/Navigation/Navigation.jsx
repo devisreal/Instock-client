e6e9a15854796bf6fb1aef62a935f4bfcc55743b
Israel Akinoso <akinosoissy@gmail.com>
2025-03-22 13:04:32 +0000

    Implement the Footer component.

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,13 @@
 import "./Navigation.scss";
 import Logo from "../../assets/logo/InStock-Logo.svg";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
     <section className="nav">
-      <img src={Logo} alt="InStock Logo" className="nav__logo" />
+      <Link to="/">
+        <img src={Logo} alt="InStock Logo" className="nav__logo" />
+      </Link>
       <div className="nav__container">
         <NavLink to="/warehouses" className="nav__title">
           Warehouses
